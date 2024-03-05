f1cd908cc0625895a8b4272f0c7a56c82c1e4f09
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2024-03-05 23:32:52 +0530

    changes

--- code.ts
+++ code.ts
@@ -1,4 +1,27 @@
-
 figma.showUI(__html__);
-
 figma.ui.resize(400, 600);
+
+function clone(val) {
+  return JSON.parse(JSON.stringify(val));
+}
+
+figma.ui.onmessage = (message) => {
+  let i = 0;
+
+  message.colors.map((color: RGB) => {
+    const rect: RectangleNode = figma.createRectangle();
+    rect.resize(100, 100);
+    const fills = clone(rect.fills);
+    if (fills[0].type === "SOLID") {
+      fills[0] = figma.util.solidPaint(color, fills[0]);
+      rect.fills = fills;
+    }
+    rect.x = rect.x + i;
+
+    i = i + 100;
+
+    figma.viewport.scrollAndZoomIntoView([rect]);
+  });
+
+  figma.closePlugin();
+};
